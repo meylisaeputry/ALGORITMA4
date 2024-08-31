ff64d6ad4d35100184dfe4f6717c812ddfb008a6
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-31 20:11:12 +0700

    37 array asosiatif

---
+++ 37 array asosiatif .php
@@ -0,0 +1,14 @@
+<?php
+// membuat array asosiatif
+$artikel = [
+    "judul" => "Belajar Pemrograman PHP",
+    "penulis" => "meylisa eka putry",
+    "view" => 128
+];
+
+// mencetak isi array assosiatif
+echo "<h2>".$artikel["judul"]."</h2>";
+echo "<p>oleh: ".$artikel["penulis"]."</p>";
+echo "<p>View: ".$artikel["view"]."</p>";
+
+echo "<p><strong><i>By Meylisa eka putry</strong>";
