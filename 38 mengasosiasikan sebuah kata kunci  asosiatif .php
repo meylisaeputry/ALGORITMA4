6c01f97b4b12ee1436691b595d590f49e6b4bfd4
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-31 20:19:07 +0700

    38 mengasosiasikan sebuah kata kunci aosiatif

---
+++ 38 mengasosiasikan sebuah kata kunci  asosiatif .php
@@ -0,0 +1,10 @@
+<?php
+$email["subjek"] = "Apa Kabar?";
+$email["pengirim"] = "lisa@meylisa eka putry.com";
+$email["isi"] = "Apa kabar meylisa? sudah lama kita tidak berjumpa";
+
+echo "<pre>";
+print_r($email);
+echo "</pre>";
+
+echo "<p><strong><i>By Meylisa eka putry</strong>";
