da60c80c52045b67798e073b5ab9964d90279544
meylisaeputry <putrymeylisa315@gmail.com>
2024-08-31 11:22:59 +0700

    03 erulangan do atau while php

---
+++ 03 perulangan do atau while .php
@@ -0,0 +1,12 @@
+<?php 
+
+$ulangi = 10;
+
+do {
+    echo "<p>ini adalah perulangan ke-$ulangi</p>";
+    $ulangi--;
+} while ($ulangi > 0);
+
+echo "<p><strong><i>By Meylisa eka putry</strong>";
+
+?>
